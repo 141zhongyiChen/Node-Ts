e2e2eebc884002b81d979bfb8b2222fb7e71cd56
141zhongyiChen <923688736@qq.com>
2019-09-10 22:38:21 +0800

    feat: 编辑脚本，更改tscconfig配置

--- build/server.js
+++ build/server.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = express_1.default();
-app.get('/', function (req, res, next) {
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/', (req, res, next) => {
     res.send('node tyscript api');
 });
-app.listen(process.env.PORT || 8080, function () {
+app.listen(process.env.PORT || 8080, () => {
     console.log('Server is listening...');
 });
