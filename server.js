6dcb7e12b396d5703b6a15286013e5542d0585e6
141zhongyiChen <923688736@qq.com>
2019-09-10 22:23:36 +0800

    feat: install @types/node -g

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('node-typescript-api');
+// console.log('node-typescript-api')
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get('/', function (req, res, next) {
+    res.send('node tyscript api');
+});
+app.listen(process.env.PORT || 8080, function () {
+    console.log('Server is listening...');
+});
--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log('node-typescript-api')
+// console.log('node-typescript-api')
+
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.send('node tyscript api')
+})
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server is listening...')
+})
